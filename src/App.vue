7b34cca99d9debda862e09a8b6f54d81ce7b4e75
Mert Sincan <sincan.mert@gmail.com>
2025-01-07 17:09:47 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import { Form, type FormSubmitEvent } from '@primevue/forms';
+import { zodResolver } from '@primevue/forms/resolvers/zod';
+import { reactive, ref } from 'vue';
+import { z } from 'zod';
 
+const initialValues = reactive({
+    username: ''
+});
+
+const resolver = ref(zodResolver(
+    z.object({
+        username: z.string().min(1, { message: 'Username is required via Zod.' })
+    })
+));
+
+const onFormSubmit = (values: FormSubmitEvent) => {
+    console.log(values);
+};
 </script>
 
 <template>
-  
+  <div class="flex">
+    <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
+      <div class="flex flex-col gap-1">
+        <InputText name="username" type="text" placeholder="Username" fluid />
+        <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple">{{ $form.username.error?.message }}</Message>
+      </div>
+      <Button type="submit" severity="secondary" label="Submit" />
+    </Form>
+  </div>
 </template>
 
 <style scoped>
