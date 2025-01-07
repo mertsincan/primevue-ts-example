200189cf090cd6758d8004221b560a69f8efbf3f
Mert Sincan <sincan.mert@gmail.com>
2025-01-07 17:10:12 +0000

    Added tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Aura from '@primevue/themes/aura';
+import { InputText, Message } from 'primevue';
+import Button from 'primevue/button';
+import PrimeVue from 'primevue/config';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.component('InputText', InputText);
+app.component('Message', Message);
+app.component('Button', Button);
+
+app.mount('#app');
